00b768ad2058122a6311db98eb6ef22531d78a7e
rju <rju@purdue.edu>
2017-10-22 05:07:35 -0500

    clean

--- Angular/script.js
+++ Angular/script.js
@@ -3,8 +3,18 @@
 // Load Dependencies
 var app = angular.module('mainApp', ['ngRoute']);
 
-var totalcost = 0;
-var nmeals = 0;
+var totalcost = 49.32;
+var nmeals = 7;
+
+var allMeals = [
+       {"id":1,"name":"Panda Express","date":"Fri Oct 20 2017 00:00:00 GMT-0500 (CDT)","cost":7.81},
+       {"id":2,"name":"Noodles & I","date":"date 1","cost":9.62},
+       {"id":3,"name":"Chipotle","date":"date 1","cost":8.03},
+       {"id":4,"name":"AJ's","date":"date 1","cost":3.20},
+       {"id":5,"name":"Jimmy John's","date":"date 1","cost":5.14},
+       {"id":6,"name":"McDonald's","date":"date 1","cost":7.49},
+       {"id":7,"name":"Chipotle","date":"date 1","cost":8.03}
+     ];
 
 app.controller('mainController', function($scope) {} );
 
@@ -32,26 +42,7 @@ app.controller('dashController', function($scope, $filter) {
                $scope.itemsPerPage = 5;
                $scope.pagedItems = [];
                $scope.currentPage = 0;
-               $scope.items = [
-                               {"id":1,"name":"name 1","description":"description 1","field3":"field3 1","field4":"field4 1","field5 ":"field5 1"},
-                               {"id":2,"name":"name 2","description":"description 1","field3":"field3 2","field4":"field4 2","field5 ":"field5 2"},
-                               {"id":3,"name":"name 3","description":"description 1","field3":"field3 3","field4":"field4 3","field5 ":"field5 3"},
-                               {"id":4,"name":"name 4","description":"description 1","field3":"field3 4","field4":"field4 4","field5 ":"field5 4"},
-                               {"id":5,"name":"name 5","description":"description 1","field3":"field3 5","field4":"field4 5","field5 ":"field5 5"},
-                               {"id":6,"name":"name 6","description":"description 1","field3":"field3 6","field4":"field4 6","field5 ":"field5 6"},
-                               {"id":7,"name":"name 7","description":"description 1","field3":"field3 7","field4":"field4 7","field5 ":"field5 7"},
-                               {"id":8,"name":"name 8","description":"description 1","field3":"field3 8","field4":"field4 8","field5 ":"field5 8"},
-                               {"id":9,"name":"name 9","description":"description 1","field3":"field3 9","field4":"field4 9","field5 ":"field5 9"},
-                               {"id":10,"name":"name 10","description":"description 1","field3":"field3 10","field4":"field4 10","field5 ":"field5 10"},
-                               {"id":11,"name":"name 11","description":"description 1","field3":"field3 11","field4":"field4 11","field5 ":"field5 11"},
-                               {"id":12,"name":"name 12","description":"description 1","field3":"field3 12","field4":"field4 12","field5 ":"field5 12"},
-                               {"id":13,"name":"name 13","description":"description 1","field3":"field3 13","field4":"field4 13","field5 ":"field5 13"},
-                               {"id":14,"name":"name 14","description":"description 1","field3":"field3 14","field4":"field4 14","field5 ":"field5 14"},
-                               {"id":15,"name":"name 15","description":"description 1","field3":"field3 15","field4":"field4 15","field5 ":"field5 15"},
-                               {"id":16,"name":"name 16","description":"description 1","field3":"field3 16","field4":"field4 16","field5 ":"field5 16"},
-                               {"id":17,"name":"name 17","description":"description 1","field3":"field3 17","field4":"field4 17","field5 ":"field5 17"},
-                               {"id":18,"name":"name 18","description":"description 1","field3":"field3 18","field4":"field4 18","field5 ":"field5 18"}
-                               ];
+               $scope.items = allMeals;
                var searchMatch = function (haystack, needle) {
                if (!needle) {
                    return true;
@@ -216,6 +207,13 @@ app.controller('addmealController', function($scope, $http) {
                    console.log("Meal Name: " + $scope.mealname);
                    console.log("Meal Cost: " + $scope.mealcost);
                    console.log("Meal Date: " + $scope.mealdate);
+               
+                   allMeals.push({
+                                 "id":20,
+                                 "name":$scope.mealname,
+                                 "date":$scope.mealdate,
+                                 "cost":$scope.mealcost
+                   });
                
                    nmeals++;
                    totalcost += $scope.mealcost;
