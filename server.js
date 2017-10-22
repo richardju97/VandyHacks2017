9274ea2a7fcaa5dddb8996a8c337b8feb789fd8c
Wilfred Kwok <wilfred.k5896@gmail.com>
2017-10-21 19:48:20 -0500

    oops

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
+// server.js
 
+var express = require("express");
+var app = require("http")
+var app = express();
+var path = require('path');
+var router = express.Router();
+var http = require('http')
+
+
+router.use(function(req, res, next) {
+	console.log("/" + req.method);
+	next();
+});
+
+
+app.use('/', express.static(path.join(__dirname, '/Angular/')));
+
+app.use('/about', express.static(path.join(__dirname, '/Angular/')));
+
+app.use('/contact', express.static(path.join(__dirname, '/Angular/')));
+
+app.use("*", function(req, res){
+	res.sendFile(path + "404.html");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+	console.log("Live at Port 3000");
+});
